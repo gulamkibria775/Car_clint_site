d0d6df9f63691896b0068a2e7c323181ed2c6b7b
golamkibriya <kamalmia5052@gmail.com>
2022-11-14 23:42:23 +0600

    home page

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,44 +1,60 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import ServiceCard from './ServiceCard';
-
-
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-    const [services, setServices] = useState([]);
-    
-    useEffect( () =>{
-        fetch('http://localhost:5000/services')
-        .then(res =>res.json())
-        .then(data => setServices(data))
-    }, []);
-    console.log(services)
+  const [services, setServices] = useState([]);
+  // spinner
+  const [isLoading, setIsLoading] = useState();
 
-    return (
-        <div>
-            <div className='text-center mb-4'>
-                <p className="text-2xl font-bold text-orange-600">Services</p>
-                <h2 className="text-5xl font-semibold">Our Service Area</h2>
-                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-            </div>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                {
-                    services.slice(0,3).map(service => <ServiceCard
-                        key={service._id}
-                        service={service}
-                    ></ServiceCard>)
-                }
-              
-            </div>
-            <div className='py-5 text-center'>
-                 <Link to="/serviecsAll">
-                        <button className="btn btn-danger">See More</button>
-                    </Link>
-                    </div>
+  useEffect(() => {
+    
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data))
+      .then( ()=>setIsLoading('hidden'));
+      
+  }, []);
+  
+  console.log(services);
+
+  return (
+    <div>
+      <div className="text-center mb-4">
+        <p className="text-2xl font-bold text-orange-600">Services</p>
+        <h2 className="text-5xl font-semibold">Our Service Area</h2>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+       
+      </div>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className={isLoading}>
+          <span class="flex h-3 w-3">
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+          </span>
         </div>
-    );
+        {services.slice(0, 3).map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))
+        
+        }
+      </div>
+      
+      
+
+      <div className="py-5 text-center">
+        <Link to="/serviecsAll">
+          <button className="btn btn-danger">See More</button>
+        </Link>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Services;
