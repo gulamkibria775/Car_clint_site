cc2e6baffda26490e0d6e5a800bd941600753abc
golamkibriya <kamalmia5052@gmail.com>
2022-11-17 22:36:15 +0600

    card is created

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useTitle from '../../../hook/useTitle';
+import Consultan from '../../Consultan\'/Consultan';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
@@ -11,6 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <About></About>
             <Services></Services>
+            <Consultan></Consultan>
         </div>
     );
 };
